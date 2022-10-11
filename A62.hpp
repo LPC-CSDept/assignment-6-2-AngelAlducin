f0c826215bf40ce4cc4d928dfcf4836a12b39fdc
AngelAlducin <aualducin@gamil.com>
2022-10-11 14:49:06 -0700

    Done

--- A62.hpp
+++ A62.hpp
@@ -8,10 +8,24 @@ using namespace std;
 double nthpowerp(int n)
 {
         // return n_th power number
+        int val = 1;
+        for(int i = 0; i <n; i++)
+                val *= 2;
+        return val;
 }
 double nthpowern(int n)
 {
         // return n_th power number
+        float val = 1;
+        n *= (-1);
+        float neg;
+        for(int i = 0; i < n; i++)
+        {
+             val *= 2;
+             neg = 1 / val;   
+        }
+
+        return neg;
 }
 void printout(double result)
 {
